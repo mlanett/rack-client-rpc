65165676585f5cc5542e73dbccf7c5c49d172799
Mark Lanett <mark.lanett@gmail.com>
2011-04-10 21:37:05 -0700

    Test for subst

--- test/subst_test.rb
+++ test/subst_test.rb
@@ -6,5 +6,20 @@ require "rack/client"
 # @see https://github.com/seattlerb/minitest
 
 describe Rack::Rpc::Parser do
-  
+  it "works" do
+    Rack::Rpc::Parser.substitute.must_equal true
+  end
+end
+
+class Upcase < Rack::Rpc::Client
+  self.rack_client = Rack::Client.new { run lambda { |env| [ 200, { 'Content-Type' => 'text/plain' }, env["PATH_INFO"].upcase ] } }
+  get   "/foo/:id"
+  put   "/bar/:id"
+end
+
+describe Rack::Rpc::Client do
+  it "supports rpc-client calls" do
+    Upcase.new.foo(1).must_equal "/FOO/1"
+    Upcase.new.bar(1).must_equal "/BAR/1"
+  end
 end
