afa58cd3d817afe1bdc4bc61264133c26012602e
Mark Lanett <mark.lanett@gmail.com>
2011-04-10 21:33:55 -0700

    Clean up

--- test/client_test.rb
+++ test/client_test.rb
@@ -5,18 +5,14 @@ require "rack/client"
 # @see http://bfts.rubyforge.org/minitest/
 # @see https://github.com/seattlerb/minitest
 
+class Upcase < Rack::Rpc::Client
+  self.rack_client = Rack::Client.new { run lambda { |env| [ 200, { 'Content-Type' => 'text/plain' }, env["PATH_INFO"].upcase ] } }
+  get   "/foo", :name => "foos"
+  post  "/bar", :name => "new_bar"
+end
+
 describe Rack::Rpc::Client do
-  
-  class Upcase < Rack::Rpc::Client
-    self.rack_client = Rack::Client.new { run lambda { |env| [ 200, { 'Content-Type' => 'text/plain' }, env["PATH_INFO"].upcase ] } }
-    get   "/foo", :name => "foos"
-    get   "/foo/:id"
-    post  "/bar", :name => "new_bar"
-    put   "/bar/:id"
-  end
-  
   it "supports rpc-client calls" do
     Upcase.new.foos.must_equal "/FOO"
   end
-  
 end
