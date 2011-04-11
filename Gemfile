953e2bc9d81df82483e7a16a3d77bd1bbb754317
Mark Lanett <mark.lanett@gmail.com>
2011-04-10 21:51:02 -0700

    Add tests for self using thin

--- Gemfile
+++ Gemfile
@@ -8,4 +8,5 @@ gem "em-http-request", :git => "git://github.com/igrigorik/em-http-request.git"
 group :development, :test do
   gem "minitest"
   gem "ruby-debug19" if RUBY_VERSION =~ /^1\.9/
+  gem "thin"
 end
---
+++ test/thin_test.rb
@@ -0,0 +1,22 @@
+require File.expand_path( "../helper", __FILE__ )
+require "rack/client"
+require "thin"
+
+SelfServer = Rack::Builder.app do
+  map "/ab" { run lambda { |env| [ 200, { 'Content-Type' => 'text/plain' }, Time.now.to_s ] } }
+end
+
+class SelfClient < Rack::Rpc::Client
+  self.rack_client = Rack::Client.new { run Rack::Client::Handler::EmHttp }
+  get   "http://localhost:3000/ab"
+end
+
+describe Rack::Rpc do
+  it "can be used to make requests to self using thin" do
+    go do
+      Thin::Server.start( "0.0.0.0", 3000, SelfServer )
+      client = SelfClient.new
+      a = client.ab
+    end
+  end
+end
