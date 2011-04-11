54e5630371d064a3138d1d09978a2067208a42a4
Mark Lanett <mark.lanett@gmail.com>
2011-04-10 20:10:13 -0700

    Client test

---
+++ test/client_test.rb
@@ -0,0 +1,22 @@
+require File.expand_path( "../helper", __FILE__ )
+require "rack/client"
+
+# Uses minitest
+# @see http://bfts.rubyforge.org/minitest/
+# @see https://github.com/seattlerb/minitest
+
+describe Rack::Rpc::Client do
+  
+  class Upcase < Rack::Rpc::Client
+    self.rack_client = Rack::Client.new { run lambda { |env| [ 200, { 'Content-Type' => 'text/plain' }, env["PATH_INFO"].upcase ] } }
+    get   "/foo", :name => "foos"
+    get   "/foo/:id"
+    post  "/bar", :name => "new_bar"
+    put   "/bar/:id"
+  end
+  
+  it "supports rpc-client calls" do
+    Upcase.new.foos.must_equal "/FOO"
+  end
+  
+end
